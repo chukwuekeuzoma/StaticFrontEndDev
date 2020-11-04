8a9ac74c1416c35ab844adda626a9df32eda2348
Martins <67004169+chukwuekeuzoma@users.noreply.github.com>
2020-11-04 17:30:50 +0100

    massage

--- buger/buger.js
+++ buger/buger.js
@@ -1,1 +1,8 @@
-// document.getElementsByClassName("services")[]
+// const HoverEffect = document.getElementById("DropDown");
+// const ClassBar = document.querySelector(".Bar");
+// console.log(HoverEffect);
+// HoverEffect.style.hover= ClassBar;
+// function showModal() { alert("yippr") }
+// const HoverEffect = document.getElementsByClassName(".fa");
+// const Bar = document.getElementsByClassName("sub-menu");
+// console.log(HoverEffect);

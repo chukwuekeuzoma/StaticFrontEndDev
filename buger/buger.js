1cb0e66baca52ad96ab31be642b9a5d2ff260893
Martins <67004169+chukwuekeuzoma@users.noreply.github.com>
2020-11-05 14:51:47 +0100

    massage

--- buger/buger.js
+++ buger/buger.js
@@ -6,3 +6,13 @@
 // const HoverEffect = document.getElementsByClassName(".fa");
 // const Bar = document.getElementsByClassName("sub-menu");
 // console.log(HoverEffect);
+function SubMenu(){
+  var expandableNavBar = document.getElementById("sub-menu");
+  let status ="";
+  
+  if (expandableNavBar.style.display === "none" || !expandableNavBar.style.display){ status = "block"}
+  else status = "none"
+//   console.log(status)
+//   console.log(expandableNavBar.style.display)
+  expandableNavBar.style.display =status;
+}
